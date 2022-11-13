51ad980287aa7b56de0bf8b6a84e746b61c60674
yubeenso <youbin0105@gmail.com>
2022-11-13 23:17:29 +0900

    chapter 3 commmit 4

--- 3. JAVASCRIPT ON THE BROWSER/3. JAVASCRIPT ON THE BROWSER/3-1. momentum/app.js
+++
@@ -1,7 +0,0 @@
-const title = document.getElementById("title");     // JS가 HTML로부터 id title에 해당하는 요소를 가져오게 하는 함수
-console.log(title);
-console.dir(title);
-console.log(title.id);
-console.log(title.className);
-
-title.innerText = "Got you!";
