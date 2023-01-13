9e91c67307ce5ac246a1b260c88af57b6c451407
yubeenso <youbin0105@gmail.com>
2023-01-14 02:05:40 +0900

    set full screen background video

--- Wrap UP Myself/js/background.js
+++ Wrap UP Myself/js/background.js
@@ -1,11 +1,11 @@
-const videos = ["night.mp4", "christmas.mp4", "orora.mp4", "river.mp4", "halloween.mp4"];
+const videos = ["night", "christmas", "orora", "river", "halloween"];
 
 const chosenVideo = videos[Math.floor(Math.random() * videos.length)];
 
 const bgVideoSource = document.createElement("source");
 
-bgVideoSource.src = `video/${chosenVideo}`;     // video 폴더 하부에 있는 이미지들이어야 한다.
-bgVideoSource.type = `video/mp4`
+bgVideoSource.src = `video/${chosenVideo}.mp4`;     // video 폴더 하부에 있는 이미지들이어야 한다.
+bgVideoSource.type = `video/mp4`;
 
 
 const bgVideo = document.querySelector("video");
