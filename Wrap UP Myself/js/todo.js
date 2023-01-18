7be7ee139290f225b02b93a3c0c027328573b06f
yubeenso <youbin0105@gmail.com>
2023-01-18 23:09:08 +0900

    add feelings

--- Wrap UP Myself/js/todo.js
+++ Wrap UP Myself/js/todo.js
@@ -3,12 +3,18 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
+// const completed_Key = "toDoCompleted";
 
 let toDos = [];     // array나 object 자료형은 const로 선언해도 내부의 값을 할당하는 것은 할 수 있다. but const 특성상 자료형 자체를 새로 할당하는 것은 불가하다.
+// let completion = [];
 
 function saveToDos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
+
+// function saveCompletion() {
+//     localStorage.setItem(completed_Key, JSON.stringify(completion));
+// }
 
 function deleteToDo(event){
     const li = event.target.parentElement;
@@ -17,27 +23,38 @@ function deleteToDo(event){
     saveToDos();
 }
 
-function completeToDo(event){
-    const li = event.target.parentElement;    // li의 button이 아닌 span(todoInput.value 즉, newTodo)에만 접근할 수 있게 하기 위해서
-    const span = toDoList.querySelector("span")
-    span.style = "text-decoration:line-through;"
-    li.removeChild(toDoList.querySelector("button:first-of-type"));
-} 
+// function completeToDo(event){
+//     const li = event.target.parentElement;
+//     const checkButton = event.target;
+//     span = checkButton.previousSibling;
+//     span.style = "text-decoration:line-through";
+//     checkButton.remove();
+//     completion.push(li.id);
+//     saveCompletion();
+    
+    // const li = event.target.parentElement;
+    // const span = toDoList.querySelector("span");
+    // span.style = "text-decoration:line-through;"
+    // li.removeChild(toDoList.querySelector("button:first-of-type"));
+    // 위 4줄처럼 하면 li가 하나일 경우에만 정상적으로 작동함. 여러개일 경우에 아래 것 먼저 check하려고 하면 
+    // Uncaught DOMException: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node. 오류가 남. 부모, 자식, 형제 노드 간의 관계를 잘 파악하고 있어야 함.
+    // https://hianna.tistory.com/712
+// }
 
 function paintToDo(newTodo){     // 화면에 그리기 위한 함수
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
     span.innerText = newTodo.text;
-    const completeButton = document.createElement("button");     // element를 여러 개 만들고 싶으면 변수도 그에 맞는 개수로 만들어야 함.
-    completeButton.innerText = "✅";
-    completeButton.addEventListener("click", completeToDo);
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "❎";
-    deleteButton.addEventListener("click", deleteToDo);
+    // const completeButton = document.createElement("button");     // element를 여러 개 만들고 싶으면 변수도 그에 맞는 개수로 만들어야 함.
+    // completeButton.innerText = "✅";
+    // completeButton.addEventListener("click", completeToDo);
+    const button = document.createElement("button");
+    button.innerText = "🗑️";
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);     // <li>는 <span>이라는 자식을 가지게 되었다.
-    li.append(completeButton);
-    li.append(deleteButton);
+    // li.append(completeButton);
+    li.append(button);
     toDoList.appendChild(li);
 }
 
@@ -63,3 +80,11 @@ if (savedToDos !== null){
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
+// const completedToDos = localStorage.getItem("toDoCompleted");
+
+// if (completedToDos !== null) {
+//     const parsedCompletion = JSON.parse(completedToDos);
+//     completion = parsedCompletion;
+//     parsedCompletion.forEach(paintToDo);
+// } 
